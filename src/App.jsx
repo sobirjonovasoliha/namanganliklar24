63033d6e3cd42c1595808b0573068e6935fa7c76
Sobirjonova Soliha <sobirjonovasoliha15@gmail.com>
2025-01-13 19:56:05 +0500

    navbar-logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1>salom</h1>
+    <Navbar/>
     </>
   )
 }
