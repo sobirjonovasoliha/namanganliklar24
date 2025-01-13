a21bdd68f75e322881372fb0b4c739f2bd9f1a76
Sobirjonova Soliha <sobirjonovasoliha15@gmail.com>
2025-01-13 19:12:26 +0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.js'
+
+import { BrowserRouter, Route, Routes } from 'react-router'
+
+createRoot(document.getElementById('root')).render(
+ <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App/>}></Route>
+    </Routes>
+ </BrowserRouter>
+)
